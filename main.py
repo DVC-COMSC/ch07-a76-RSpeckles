7bd2943d852af0552e6a4302459d9a8b1bc6bd01
RSpeckles <rchenspeckman@gmail.com>
2022-10-08 14:25:23 -0700

    Completed assignment

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
-# ******************************
-# Make your Code
-# ******************************
+#Checks if num1 is shorter or equal in length to num2. If it is, it will be added first
+#if not num2 will be added first
+if len(num1) <= len(num2):
+    num3 = num1 + num2
+else:
+    num3 = num2 + num1
 
-# print (num3) 
+print (num3) 
